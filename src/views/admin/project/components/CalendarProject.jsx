d3c758221800aa29b6a244d7e600e00139a48336
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2023-12-20 22:24:12 +0700

    update

--- src/views/admin/project/components/CalendarProject.jsx
+++ src/views/admin/project/components/CalendarProject.jsx
@@ -238,169 +238,165 @@ const CalendarProject = () => {
         textAlign: "center",
       }}
     >
-      <Modal isOpen={isOpen || openCalendar} onClose={onClose}>
-        <ModalContent>
-          <Flex width={{ base: "90%", sm: "80%", md: "25%", lg: "25%", xl: "25%" }} gap="15px" marginBottom="20px">
-            <Box width="auto">
-              <Box>
-                <Link to="/admin/list-task">
-                  <Button
-                    backgroundColor="#422AFB"
-                    _hover={{ color: "black", backgroundColor: "gray.100" }}
-                    color="white"
-                  >
-                    Show all task
-                  </Button>
-                </Link>
-              </Box>
-            </Box>
-            <Box width="auto">
-              <Box>
-                <Button
-                  backgroundColor="#422AFB"
-                  _hover={{ color: "black", backgroundColor: "gray.100" }}
-                  color="white"
-                  onClick={() => setOpenModal(true)}
-                >
-                  Create new task
-                </Button>
-              </Box>
-            </Box>
-          </Flex>
+      <Flex width={{ base: "90%", sm: "80%", md: "25%", lg: "25%", xl: "25%" }} gap="15px" marginBottom="20px">
+        <Box width="auto">
+          <Box>
+            <Link to="/admin/list-task">
+              <Button
+                backgroundColor="#422AFB"
+                _hover={{ color: "black", backgroundColor: "gray.100" }}
+                color="white"
+              >
+                Show all task
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+        <Box width="auto">
+          <Box>
+            <Button
+              backgroundColor="#422AFB"
+              _hover={{ color: "black", backgroundColor: "gray.100" }}
+              color="white"
+              onClick={() => setOpenModal(true)}
+            >
+              Create new task
+            </Button>
+          </Box>
+        </Box>
+      </Flex>
 
-          <DragDropCalendar
-            events={userEvents}
-            localizer={localizer}
-            startAccessor="start"
-            endAccessor="end"
-            onEventDrop={onEventDrop}
-            onEventResize={onEventResize}
-            onSelectEvent={handleSelectEvent}
-            onSelectSlot={handleSelectSlot}
-            selectable
-            resizable
-            style={{
-              height: "500px",
-              width: "100%",
-              margin: "0 auto",
-              color: "#422AFB",
+      <DragDropCalendar
+        events={userEvents}
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        onSelectEvent={handleSelectEvent}
+        onSelectSlot={handleSelectSlot}
+        selectable
+        resizable
+        style={{
+          height: "500px",
+          width: "100%",
+          margin: "0 auto",
+          color: "#422AFB",
+        }}
+      />
+      <Modal isOpen={isOpen || openModal} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Task</ModalHeader>
+          <ModalCloseButton
+            onClick={() => {
+              setOpenModal(false);
             }}
           />
-          <Modal isOpen={isOpen || openModal} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Add New Task</ModalHeader>
-              <ModalCloseButton
-                onClick={() => {
-                  setOpenModal(false);
-                }}
+          <ModalBody padding="5%">
+            {/* title  */}
+            <FormControl>
+              <FormLabel>Title</FormLabel>
+              <Input
+                placeholder="Title"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                autoFocus
               />
-              <ModalBody padding="5%">
-                {/* title  */}
-                <FormControl>
-                  <FormLabel>Title</FormLabel>
-                  <Input
-                    placeholder="Title"
-                    name="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    autoFocus
-                  />
-                </FormControl>
+            </FormControl>
 
-                {/* Start Date  */}
-                <FormControl>
-                  <FormLabel>Start Date</FormLabel>
-                  <Input
-                    name="start-date"
-                    type="date"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  />
-                </FormControl>
+            {/* Start Date  */}
+            <FormControl>
+              <FormLabel>Start Date</FormLabel>
+              <Input
+                name="start-date"
+                type="date"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </FormControl>
 
-                {/* End Date  */}
-                <FormControl>
-                  <FormLabel>End Date</FormLabel>
-                  <Input
-                    name="end-date"
-                    type="date"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                  />
-                </FormControl>
-              </ModalBody>
+            {/* End Date  */}
+            <FormControl>
+              <FormLabel>End Date</FormLabel>
+              <Input
+                name="end-date"
+                type="date"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
 
-              <FormControl width="90%" margin="auto">
-                <FormLabel>Start Time</FormLabel>
-                <Input
-                  name="start-time"
-                  type="time"
-                  value={startTime}
-                  onChange={(e) => setStartTime(e.target.value)}
-                />
-              </FormControl>
+          <FormControl width="90%" margin="auto">
+            <FormLabel>Start Time</FormLabel>
+            <Input
+              name="start-time"
+              type="time"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </FormControl>
 
-              <FormControl width="90%" margin="auto">
-                <FormLabel>End Time</FormLabel>
-                <Input
-                  name="end-time"
-                  type="time"
-                  value={endTime}
-                  onChange={(e) => setEndTime(e.target.value)}
-                />
-              </FormControl>
+          <FormControl width="90%" margin="auto">
+            <FormLabel>End Time</FormLabel>
+            <Input
+              name="end-time"
+              type="time"
+              value={endTime}
+              onChange={(e) => setEndTime(e.target.value)}
+            />
+          </FormControl>
 
-              <FormControl width="90%" margin="auto">
-                <FormLabel>PM/AM</FormLabel>
-                <Select value={amPm} onChange={(e) => setAmPm(e.target.value)}>
-                  <option value="AM">AM</option>
-                  <option value="PM">PM</option>
-                </Select>
-              </FormControl>
+          <FormControl width="90%" margin="auto">
+            <FormLabel>PM/AM</FormLabel>
+            <Select value={amPm} onChange={(e) => setAmPm(e.target.value)}>
+              <option value="AM">AM</option>
+              <option value="PM">PM</option>
+            </Select>
+          </FormControl>
 
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3} onClick={() => handleSubmitEvent()}>
-                  {showDeleteBtn ? "Edit" : "Submit"}
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={() => handleSubmitEvent()}>
+              {showDeleteBtn ? "Edit" : "Submit"}
+            </Button>
+
+            {showDeleteBtn && (
+              <>
+                <Button variant="ghost" onClick={onOpen} bg={"red.400"}>
+                  Delete
                 </Button>
+                <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+                  <AlertDialogOverlay>
+                    <AlertDialogContent>
+                      <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                        Delete Task
+                      </AlertDialogHeader>
 
-                {showDeleteBtn && (
-                  <>
-                    <Button variant="ghost" onClick={onOpen} bg={"red.400"}>
-                      Delete
-                    </Button>
-                    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-                      <AlertDialogOverlay>
-                        <AlertDialogContent>
-                          <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                            Delete Task
-                          </AlertDialogHeader>
+                      <AlertDialogBody>Bạn có chắc muốn xóa task</AlertDialogBody>
 
-                          <AlertDialogBody>Bạn có chắc muốn xóa task</AlertDialogBody>
-
-                          <AlertDialogFooter>
-                            <Button ref={cancelRef} onClick={onClose}>
-                              Cancel
-                            </Button>
-                            <Button
-                              colorScheme="red"
-                              onClick={() => {
-                                onClose();
-                                handleDeleteEvent();
-                              }}
-                              ml={3}
-                            >
-                              Delete
-                            </Button>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </AlertDialogOverlay>
-                    </AlertDialog>
-                  </>
-                )}
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
+                      <AlertDialogFooter>
+                        <Button ref={cancelRef} onClick={onClose}>
+                          Cancel
+                        </Button>
+                        <Button
+                          colorScheme="red"
+                          onClick={() => {
+                            onClose();
+                            handleDeleteEvent();
+                          }}
+                          ml={3}
+                        >
+                          Delete
+                        </Button>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialogOverlay>
+                </AlertDialog>
+              </>
+            )}
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </div>
